d5fcde49e4debc3cd5430d9a8e6031f4ad0d766a
Paulo Melo <peimelo@gmail.com>
2018-02-28 20:17:40 -0300

    Add Router

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { BrowserRouter, Link } from 'react-router-dom'
 import * as CounterActions from '../actions'
 import Header from '../components/Header'
 import '../blog.css'
 import * as BlogAPI from '../utils/BlogAPI'
 import Moment from 'react-moment';
+
 // import Counter from '../components/Counter';
 
 class App extends Component {
@@ -27,69 +29,80 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="container">
-          <Header title="Readable" />
+      <BrowserRouter>
+        <div>
+          <div className="container">
+            <Header title="Readable"/>
 
-          <div className="nav-scroller py-1 mb-2">
-            <nav className="nav d-flex justify-content-between">
-              {this.state.categories.map((categorie) => (
-                <a key={categorie.name} className="p-2 text-muted" href="#">{categorie.name}</a>
-              ))}
-            </nav>
+            <div className="nav-scroller py-1 mb-2">
+              <nav className="nav d-flex justify-content-between">
+                {this.state.categories.map((categorie) => (
+                  <Link
+                    key={categorie.name}
+                    className="p-2 text-muted"
+                    to={categorie.path}
+                  >
+                    {categorie.name}
+                  </Link>
+                ))}
+              </nav>
+            </div>
+
           </div>
 
-        </div>
+          <main role="main" className="container">
+            <div className="row">
+              <div className="col-md-8 blog-main">
 
-        <main role="main" className="container">
-          <div className="row">
-            <div className="col-md-8 blog-main">
+                {this.state.posts.map((post) => (
+                  <div key={post.id} className="blog-post">
+                    <Link to={post.id}>
+                      <h2 className="blog-post-title">{post.title}</h2>
+                    </Link>
+                    <p className="blog-post-meta">
+                      <Moment format="DD/MM/YYYY">
+                        {post.timestamp}
+                      </Moment> by {post.author}
+                    </p>
 
-              {this.state.posts.map((post) => (
-                <div key={post.id} className="blog-post">
-                  <h2 className="blog-post-title">{post.title}</h2>
+                    <p>{post.body}</p>
+                  </div>
+                ))}
 
-                  <p className="blog-post-meta">
-                    <Moment format="DD/MM/YYYY">
-                      {post.timestamp}
-                    </Moment> by <a href="#">{post.author}</a>
-                  </p>
+              </div>
 
-                  <p>{post.body}</p>
+              <aside className="col-md-4 blog-sidebar">
+
+                <div className="p-3">
+                  <h4 className="font-italic">Order by</h4>
+                  <ol className="list-unstyled">
+                    <li><a href="#">Vote Score</a></li>
+                    <li><a href="#">Created at</a></li>
+                  </ol>
                 </div>
-              ))}
+              </aside>
 
             </div>
 
-            <aside className="col-md-4 blog-sidebar">
+          </main>
 
-              <div className="p-3">
-                <h4 className="font-italic">Order by</h4>
-                <ol className="list-unstyled">
-                  <li><a href="#">Vote Score</a></li>
-                  <li><a href="#">Created at</a></li>
-                </ol>
-              </div>
-            </aside>
+          <footer className="blog-footer">
+            <p>Blog template built for <a
+              href="https://getbootstrap.com/">Bootstrap</a> by <a
+              href="https://twitter.com/mdo">@mdo</a>.</p>
+            <p>
+              <a href="#">Back to top</a>
+            </p>
+          </footer>
 
-          </div>
-
-        </main>
-
-        <footer className="blog-footer">
-          <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-          <p>
-            <a href="#">Back to top</a>
-          </p>
-        </footer>
-
-        {/*<Counter*/}
+          {/*<Counter*/}
           {/*value={this.props.counter}*/}
           {/*onIncrement={() => this.props.actions.increment()}*/}
           {/*onDecrement={() => this.props.actions.decrement()}*/}
-        {/*/>*/}
+          {/*/>*/}
 
-      </div>
+        </div>
+      </BrowserRouter>
     );
   }
 }
