584511e2ba559d58d74e1e981df72131d520ffde
Paulo Melo <peimelo@gmail.com>
2018-04-02 09:50:10 -0300

    Add icons

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Moment from 'react-moment';
-import StarIcon from 'react-icons/lib/fa/star'
+import CalendarIcon from 'react-icons/lib/fa/calendar';
+import CommentsIcon from 'react-icons/lib/fa/comments';
+import StarIcon from 'react-icons/lib/fa/star';
+import TagIcon from 'react-icons/lib/fa/tag';
+import User from 'react-icons/lib/fa/user';
 
 function Posts({ posts }) {
   return (
@@ -13,11 +17,14 @@ function Posts({ posts }) {
             <h2 className="blog-post-title">{post.title}</h2>
           </Link>
           <p className="blog-post-meta">
+            <CalendarIcon size={15} />&nbsp;
             <Moment format="DD/MM/YYYY">
               {post.timestamp}
             </Moment>&nbsp;
-            | by {post.author}&nbsp;
-            | {post.voteScore} <StarIcon size={15}/>
+            | <User size={15} /> by {post.author}&nbsp;
+            | <StarIcon size={15} /> {post.voteScore}&nbsp;
+            | <TagIcon size={15} /> {post.category}
+            | <CommentsIcon size={15} /> {post.commentCount}
           </p>
 
           <p>{post.body}</p>
