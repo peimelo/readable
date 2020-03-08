99a023c32b913b4b3ec4182d19b49cdbb524f18e
Paulo Melo <peimelo@gmail.com>
2020-03-08 15:15:47 -0300

    Ajuste

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux'
-
 import App from './App';
-import reducer from './reducers'
-import * as serviceWorker from './serviceWorker';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import reducer from './reducers';
+import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
-);
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
