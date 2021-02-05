387f0624f41a3b721833403fb81b6304b8d17aa8
Paulo Melo <peimelo@gmail.com>
2021-02-05 09:56:09 -0300

    Deploy

--- api/index.js
+++ api/index.js
@@ -121,7 +121,7 @@ app.get('/*', (req, res) => {
 });
 
 if (process.env.HEROKU || process.env.NODE_ENV !== 'production') {
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
 
   app.listen(port, () => {
     console.log('Server listening on port %s, Ctrl+C to stop', port)
